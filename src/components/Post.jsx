aef5305efd258bfa2bb65406def98e7d63504c71
Edson Rodrigues <edsonr.dev@gmail.com>
2023-04-13 15:40:26 -0300

    Post component finished

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,37 @@
+import styles from "./Post.module.css";
+
 export function Post(props) {
   return (
-    <div>
-      <h1>{props.author}</h1>
-      <p>{props.content}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            src="https://avatars.githubusercontent.com/u/79758730?s=400&u=d84869edc03239f2a54f67fd25e011493a43196e&v=4"
+          />
+
+          <div className={styles.authorInfo}>
+            <strong>Edson Rodrigues</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="13 de abril, às 14:54" dateTime="2023-04-13 14:54:39">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>
+          👉 <a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto</a>&nbsp;
+          <a href="#">#nlw</a>&nbsp;
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
   );
 }
